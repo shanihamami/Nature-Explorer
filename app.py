42b68afb8c09d3fb1aaad79c2d7eab648c6cf5a7
shanihamami <shanihamami@gmail.com>
2024-09-14 10:23:14 +0300

    Changing port for deployment

--- app.py
+++ app.py
@@ -693,6 +693,7 @@ def api_get_news():
     return jsonify(news_titles)  # החזרת הנתונים כ-JSON
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
